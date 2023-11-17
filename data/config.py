939a854cb7c5033fb108144586e2f2b0e1f76063
Sar-dor <sardorernazarov4555@gmail.com>
2023-11-17 20:59:48 +0500

    commit

--- data/config.py
+++ data/config.py
@@ -15,4 +15,4 @@ import os
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
 ADMINS = [1004403881]  # adminlar ro'yxati
-GROUP = "@varfootballgroup"
+GROUP = ["@varfootballgroup"]
